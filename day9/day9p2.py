f45094ad6d32baf796ac447ac0c03e8aa0f65374
Karl Törnblom Bartholf <karltb@kth.se>
2022-12-09 15:27:48 +0100

    Continue work on day 9 part 2

--- day9/day9p2.py
+++ day9/day9p2.py
@@ -3,17 +3,9 @@ from math import sqrt as sqrt
 
 class Driver:
     def __init__(self):
-        self.moves = []
         self.visited = []
-        self.head = Knot(None)
-        num_children = 9
-        parent = self.head
-        self.children = []
-        for _ in range(num_children):
-            knot = Knot(parent)
-            self.children.append(knot)
-            parent = knot
-        self.tail = self.children[8]
+        self.moves = []
+        self.rope = Rope()
 
     def read(self):
         with open("example.in", "r") as f:
@@ -24,32 +16,44 @@ class Driver:
 
     def execute_moves(self):
         for move in self.moves:
-            self.move_parent(move)
-            direction = move[0]
-            num_steps = move[1]
-            for _ in range(num_steps):
-                self.visit_current_cell()
-                parent_pos = self.move_head_in_direction(direction)
-                for child in self.children:
-                    parent_pos = child.parent.pos
-                    if not self.child_is_adjacent(child):
-                        parent_pos = self.move_child(child, parent_pos)
-                    parent_pos = parent_pos
-            self.visit_current_cell()
-        self.normalize_visited()
+            self.rope.move_head(move)
+        visited = self.rope.visited
+        self.visited = self.normalize_visited(visited)
 
-    def move_parent(self, move):
-        direction = move[0]
-        num_steps = move[1]
-        for _ in range(num_steps):
-            pass
+    def normalize_visited(self, visited):
+        res = []
+        [res.append(pos) for pos in visited if pos not in res]
+        return res
 
-    def move_child(self, child, pos):
-        old_pos = child.pos
-        child.move_to(pos)
-        return old_pos
+    def print_moves(self):
+        for pos in self.visited:
+            print(pos)
 
-    def child_is_adjacent(self, child):
+    def get_result(self):
+        return len(self.visited)
+
+
+class Rope:
+    def __init__(self):
+        self.visited = []
+        num_knots = 10
+        self.knots = [Knot(i) for i in range(num_knots)]
+        parent = None
+        for knot in self.knots:
+            knot.parent = parent
+            parent = knot
+        child = None
+        for knot in reversed(self.knots):
+            knot.child = child
+            child = knot
+
+        for knot in self.knots:
+            print("Knot number: " + str(knot) + ", parent: " + str(knot.parent) + ", child: " + str(knot.child))
+        self.head = self.knots[0]
+        tail_index = num_knots - 1
+        self.tail = self.knots[tail_index]
+
+    def child_is_adjacent_to_parent(self, child):
         parent = child.parent
         dist = int(self.distance(parent, child))
         return dist < 2
@@ -62,43 +66,51 @@ class Driver:
     def visit_current_cell(self):
         self.visited.append(self.tail.pos)
 
-    def move_tail(self, pos):
-        self.tail.move_to(pos)
-
-    def head_is(self, pos):
-        return self.head.pos == pos
-
     def distance(self, parent, child):
+        parent_x = parent.x
+        parent_y = parent.y
         child_x = child.x
         child_y = child.y
-        parent_y = parent.x
-        parent_y = parent.y
-        dx = abs(parent_y - child_x)
+        dx = abs(parent_x - child_x)
         dy = abs(parent_y - child_y)
         return int(max(dx, dy))
 
     def move_head_in_direction(self, direction):
-        curr_pos = self.head.pos
-        curr_x = curr_pos[0]
-        curr_y = curr_pos[1]
+        pos = self.head.pos
+        x = pos[0]
+        y = pos[1]
         # Right
         if direction == "R":
-            new_pos = [curr_x + 1, curr_y]
+            new_pos = [x + 1, y]
         # Up
         elif direction == "U":
-            new_pos = [curr_x, curr_y + 1]
+            new_pos = [x, y + 1]
         # Left
         elif direction == "L":
-            new_pos = [curr_x - 1, curr_y]
+            new_pos = [x - 1, y]
         # Down
         else:
-            new_pos = [curr_x, curr_y - 1]
+            new_pos = [x, y - 1]
         self.head.move_to(new_pos)
-        return curr_pos
+        return pos
 
-    def print_moves(self):
-        for pos in self.visited:
-            print(pos)
+    def move_head(self, move):
+        direction = move[0]
+        num_steps = move[1]
+        for _ in range(num_steps):
+            pos = self.move_head_in_direction(direction)
+            self.move_child(self.head, pos)
+            #self.print_matrix()
+
+    def move_child(self, knot, parent_pos):
+        if knot.child == None:
+            self.visit_current_cell()
+            return
+        pos = knot.pos
+        child = knot.child
+        if not self.child_is_adjacent_to_parent(child):
+            child.move_to(parent_pos)
+        return self.move_child(child, pos)
 
     def print_matrix(self):
         height = 5
@@ -113,32 +125,24 @@ class Driver:
             for col in row:
                 print(col, end="")
             print("\n", end="")
-
-    def get_result(self):
-        return len(self.visited)
-
-
-""" class Rope:
-    def __init__(self):
-        self.head = Knot(None)
-        num_children = 9
-        self.children = []
-        for n in range(num_children):
-            knot = Knot(n)
-            self.children.append(knot) """
+        print("")
 
 
 class Knot:
-    def __init__(self, parent):
+    def __init__(self, num):
+        self.num = num
         self.x = 0
         self.y = 0
-        self.parent = parent
+        self.child = None
         self.pos = [self.x, self.y]
 
     def move_to(self, pos):
         self.x = pos[0]
         self.y = pos[1]
         self.pos = [self.x, self.y]
+
+    def __str__(self):
+        return str(self.num)
 
 
 """ class Grid:
